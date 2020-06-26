fa15dfd39984d4fa3c4df611ce6922788b839596
Leonardo Razovic <leorazzo@gmail.com>
2020-06-26 16:25:07 +0200

    Better debug info

--- webnfc.js
+++ webnfc.js
@@ -2,18 +2,19 @@ const writer = new NDEFWriter();
 
 function write(event) {
   let name = event.srcElement.name;
-  console.log(name);
   writer
     .write(name)
     .then(() => {
       console.log(`Message ${name} written.`);
+      textField.innerHTML = "Message sent successfully";
     })
     .catch((error) => {
-      console.log(`Write failed :-( try again: ${error}.`);
+      console.error(`Write failed :-( try again: ${error}.`);
     });
 }
 
 var buttons = document.getElementsByClassName("write");
+var textField = document.getElementById("message");
 for (var i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", write);
 }

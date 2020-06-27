697c6399655ee71d0e45c5680fdf69958697a9f8
Leonardo Razovic <leorazzo@gmail.com>
2020-06-27 09:55:07 +0200

    Catch Errors in NFC permissions

--- webnfc.js
+++ webnfc.js
@@ -1,17 +1,22 @@
 const writer = new NDEFWriter();
 
 async function write(event) {
-  textField.innerHTML = "Move your smartphone closer to the receiver";
   let name = event.srcElement.name;
-  await writer
-    .write(name)
-    .then(() => {
-      console.log(`Message ${name} written.`);
-      textField.innerHTML = "Message sent successfully";
-    })
-    .catch((error) => {
-      console.error(`Write failed :-( try again: ${error}.`);
-    });
+  try {
+    textField.innerHTML = "Move your smartphone closer to the receiver";
+    await writer
+      .write(name)
+      .then(() => {
+        console.log(`Message ${name} written.`);
+        textField.innerHTML = "Message sent successfully";
+      })
+      .catch((error) => {
+        console.error(`Write failed :-( try again: ${error}.`);
+      });
+  } catch (error) {
+    textField.style.color = "red";
+    textField.innerHTML = `Error: ${error}`;
+  }
 }
 
 var buttons = document.getElementsByClassName("write");

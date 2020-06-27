ce4d3f7b82e59ba664cdc56299ed0d0272207bc7
Leonardo Razovic <leorazzo@gmail.com>
2020-06-27 09:58:09 +0200

    Update webnfc.js

--- webnfc.js
+++ webnfc.js
@@ -1,18 +1,18 @@
 const writer = new NDEFWriter();
 
-async function write(event) {
+function write(event) {
   let name = event.srcElement.name;
   try {
     textField.innerHTML = "Move your smartphone closer to the receiver";
-    await writer
-      .write(name)
-      .then(() => {
-        console.log(`Message ${name} written.`);
-        textField.innerHTML = "Message sent successfully";
-      })
+    writer.write(name).then(() => {
+      console.log(`Message ${name} written.`);
+      textField.innerHTML = "Message sent successfully";
+    });
+    /*
       .catch((error) => {
         console.error(`Write failed :-( try again: ${error}.`);
       });
+      */
   } catch (error) {
     textField.style.color = "red";
     textField.innerHTML = `Error: ${error}`;

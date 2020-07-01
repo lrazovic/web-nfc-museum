30adc8b6274ae0c9e78616802e7243b65cfd3cf7
Leonardo Razovic <leorazzo@gmail.com>
2020-07-01 17:28:44 +0200

    cleanup

--- webnfc.js
+++ webnfc.js
@@ -1,7 +1,5 @@
 async function write(event) {
   let name = event.srcElement.name;
-  console.log(event);
-  console.log(name);
   try {
     const writer = new NDEFWriter();
     textField.style.color = "green";
